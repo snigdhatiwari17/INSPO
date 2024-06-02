0d3bb8a6f2204a955063ebb2125331f07ce23a4a
snigdha tiwari <snigdha.17.tiwari@gmail.com>
2024-06-02 18:41:16 +0530

    Initial Commit

---
+++ INSPO/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  INSPO
+//
+//  Created by Snigdha Tiwari  on 02/06/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
